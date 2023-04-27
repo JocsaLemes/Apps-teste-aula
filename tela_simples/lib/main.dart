16261d5fc89fda65934efd80b7bd6f1d3983c3f9
JocsaLemes <131957074+JocsaLemes@users.noreply.github.com>
2023-04-27 06:19:01 -0300

    Update main.dart

--- tela_simples/lib/main.dart
+++ tela_simples/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Alterado no github',
       theme: ThemeData(
         // This is the theme of your application.
         //
